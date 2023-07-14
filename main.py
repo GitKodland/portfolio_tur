077eb364eb5200bfec61b84f6688559ea89fd421
GitKodland <e.rezvaya@kodland.team>
2023-07-14 14:29:24 +0300

    add files

---
+++ main.py
@@ -0,0 +1,22 @@
+# Import
+from flask import Flask, render_template,request, redirect
+
+
+
+app = Flask(__name__)
+
+# Running content page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Dynamic skills
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    return render_template('index.html', button_python=button_python)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
